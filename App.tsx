70159f52813400004ca48b4664abd56394227176
Adam-Schlichtmann <35603662+Adam-Schlichtmann@users.noreply.github.com>
2025-04-23 08:15:30 -0500

    Set up base example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.baseText}>Unstyled text</Text>
+      {Font.getLoadedFonts().map((font) => {
+        return (
+          <Text key={font} style={[styles.baseText, { fontFamily: font }]}>
+            {font}
+          </Text>
+        );
+      })}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  baseText: {
+    fontSize: 32,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
